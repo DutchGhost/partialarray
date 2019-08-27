da080f6dd3b864055fb0455bf58c328cf1385b4c
Dodo <kasper199914@gmail.com>
2019-08-27 12:22:25 +0200

    doc tests + examples

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,15 @@ impl<T, const N: usize> Default for MaybeArray<T, { N }> {
 
 impl<T, const N: usize> MaybeArray<T, { N }> {
     /// Returns the capacity of the array.
+    ///
+    /// # Examples
+    /// ```
+    /// use partialarray::MaybeArray;
+    ///
+    /// let array = MaybeArray::<i32, { 10 }>::default();
+    ///
+    /// assert_eq!(array.capacity(), 10);
+    /// ```
     #[cfg(not(miri))]
     #[inline(always)]
     pub const fn capacity(&self) -> usize {
@@ -47,6 +56,15 @@ impl<T, const N: usize> MaybeArray<T, { N }> {
     /// This element *may* be uninitialized.
     ///
     /// if `N` is 0, for all values of `offset`, this function returns a dangling pointer.
+    ///
+    /// # Examples
+    /// ```
+    /// use partialarray::MaybeArray;
+    ///
+    /// let array = MaybeArray::<i32, { 4 }>::default();
+    ///
+    /// let ptr = array.nth_ptr(1);
+    /// ```
     #[inline(always)]
     pub fn nth_ptr(&self, offset: usize) -> *const T {
         debug_assert!(offset <= self.capacity());
@@ -62,6 +80,15 @@ impl<T, const N: usize> MaybeArray<T, { N }> {
     /// This element *may* be uninitialized.
     ///
     /// if `N` is 0, for all values of `offset`, this function returns a dangling pointer.
+    ///
+    /// # Examples
+    /// ```
+    /// use partialarray::MaybeArray;
+    ///
+    /// let mut array = MaybeArray::<i32, { 4 }>::default();
+    ///
+    /// let ptr = array.nth_ptr_mut(1);
+    /// ```
     #[inline(always)]
     pub fn nth_ptr_mut(&mut self, offset: usize) -> *mut T {
         debug_assert!(offset <= self.capacity());
@@ -76,23 +103,55 @@ impl<T, const N: usize> MaybeArray<T, { N }> {
     /// Returns a pointer to the first element of the array.
     /// This element *may* be uninitialized.
     ///
-    /// If `N` is 0, the returned pointer is dangling.
+    /// If `N` is 0, the returned pointer is dangling, but aligned.
+    ///
+    /// # Examples
+    /// ```
+    /// use partialarray::MaybeArray;
+    ///
+    /// let array = MaybeArray::<String, { 10 }>::default();
+    /// assert_eq!(array.first_ptr(), array.nth_ptr(0));
+    /// ```
     #[inline(always)]
     pub fn first_ptr(&self) -> *const T {
-        self.nth_ptr(0)
+        // Using `array_ptr` here instead of `nth_ptr(0)`,
+        // since `array_ptr` does no pointer offsets,
+        // and just converts &MaybeArray<T, { N }> to `*const [T; N]`.
+        // this pointer is equal to the first element.
+        self.array_ptr() as *const T
     }
 
     /// Returns a mutable pointer to the first element of the array.
     /// This element *may* be uninitialized.
     ///
-    /// If `N` is 0, the returned pointer is dangling.
+    /// If `N` is 0, the returned pointer is dangling, but aligned.
+    ///
+    /// # Examples
+    /// ```
+    /// use partialarray::MaybeArray;
+    ///
+    /// let mut array = MaybeArray::<String, { 10 }>::default();
+    /// assert_eq!(array.first_ptr_mut(), array.nth_ptr_mut(0));
+    /// ```
     #[inline(always)]
     pub fn first_ptr_mut(&mut self) -> *mut T {
-        self.nth_ptr_mut(0)
+        // Using `array_ptr_mut` here instead of `nth_ptr(_mut0)`,
+        // since `array_ptr_mut` does no pointer offsets,
+        // and just converts &mut MaybeArray<T, { N }> to `*mut [T; N]`.
+        // this pointer is equal to the first element.
+        self.array_ptr_mut() as *mut T
     }
 
     /// Returns a pointer to the whole array.
     /// There *may* be elements in the array that are uninitialized.
+    ///
+    /// # Examples
+    /// ```
+    /// use partialarray::MaybeArray;
+    ///
+    /// let mut array = MaybeArray::<Vec<i32>, { 3 }>::default();
+    /// let ptr: *const [Vec<i32>; 3] = array.array_ptr();
+    /// ```
     #[inline(always)]
     pub const fn array_ptr(&self) -> *const [T; N] {
         &self.array as *const [MaybeUninit<T>; N] as *const [T; N]
@@ -100,31 +159,72 @@ impl<T, const N: usize> MaybeArray<T, { N }> {
 
     /// Returns a mutable pointer to the whole array.
     /// There *may* be elements in the array that are uninitialized.
+    ///
+    /// # Examples
+    /// ```
+    /// use partialarray::MaybeArray;
+    ///
+    /// let mut array = MaybeArray::<Vec<i32>, { 3 }>::default();
+    /// let ptr: *mut [Vec<i32>; 3] = array.array_ptr_mut();
+    /// ```
     #[inline(always)]
     pub fn array_ptr_mut(&mut self) -> *mut [T; N] {
         &mut self.array as *mut [MaybeUninit<T>; N] as *mut [T; N]
     }
 
-    /// Reads the element at `index`.
+    /// Reads the element at `offset`.
     /// After reading the element, it should be considered moved.
-    /// This means calling `take` more than once with the same index,
-    /// without an intermediate write to the index is invalid, unless T implements Copy.
+    ///
+    /// This means calling `take` more than once with the same offset,
+    /// without an intermediate write to the offset is invalid, unless `T` implements Copy.
+    ///
+    /// See also [`MaybeArray::write`].
     ///
     /// # Unsafe
     /// Marked unsafe because
-    /// 1) `index` is *NOT* boundschecked.
-    /// 2) The element at `index` may be uninitialized, thus reading it is invalid.
+    /// 1) `offset` is *NOT* boundschecked.
+    /// 2) The element at `offset` may be uninitialized, thus reading it is invalid.
+    ///
+    /// # Examples
+    /// ```
+    /// use partialarray::MaybeArray;
+    ///
+    /// let mut array = MaybeArray::<String, { 10 }>::default();
+    /// unsafe {
+    ///     // The 0th element is initialized.
+    ///     array.write(0, String::from("hello"));
+    ///     
+    ///     // Reading the 0th element is ok.
+    ///     assert_eq!(array.read(0), "hello");
+    /// }
+    /// ```
     #[inline(always)]
     pub unsafe fn read(&self, offset: usize) -> T {
         ptr::read(self.nth_ptr(offset))
     }
 
-    /// Sets the element at `index` to `value`.
-    /// Any previous element at `index` is overwritten,
+    /// Sets the element at `offset` to `value`.
+    /// Any previous element at `offset` is overwritten,
     /// and it's destructor will NOT run.
     ///
     /// # Unsafe
-    /// Marked unsafe because `index` is not boundschecked.
+    /// Marked unsafe because `offset` is not boundschecked.
+    ///
+    /// # Examples
+    /// ```
+    /// use partialarray::MaybeArray;
+    ///
+    /// let mut array = MaybeArray::<String, { 2 }>::default();
+    ///
+    /// unsafe {
+    ///     for offset in 0..2 {
+    ///         array.write(offset, format!("I am number {}", offset));
+    ///     }
+    ///     
+    ///     let array = MaybeArray::into_array(array);
+    ///
+    ///     assert_eq!(array, [String::from("I am number 0"), String::from("I am number 1")]);
+    /// }
     #[inline(always)]
     pub unsafe fn write(&mut self, offset: usize, value: T) {
         ptr::write(self.nth_ptr_mut(offset), value)
@@ -134,6 +234,22 @@ impl<T, const N: usize> MaybeArray<T, { N }> {
     /// # Unsafe
     /// Marked unsafe because all elements within the `MaybeArray` *MUST*
     /// be initialized. If they are not, invoking this function is UB.
+    ///
+    /// # Examples
+    /// ```
+    /// use partialarray::MaybeArray;
+    ///
+    /// let mut array = MaybeArray::<i32, { 3 }>::default();
+    ///
+    /// unsafe {
+    ///     for offset in 0..array.capacity() {
+    ///         array.write(offset, offset as i32 + 1);
+    ///     }
+    ///
+    ///     let array = MaybeArray::into_array(array);
+    ///     assert_eq!(&array, &[1, 2, 3]);
+    /// }
+    /// ```
     pub unsafe fn into_array(mut this: MaybeArray<T, { N }>) -> [T; N] {
         // We dont have to `mem::forget(this)`,
         // since the inner array contains `MaybeUninit<T>`'s,
@@ -142,6 +258,29 @@ impl<T, const N: usize> MaybeArray<T, { N }> {
         ptr::read(array_ptr)
     }
 
+    /// Returns a subslice without doing boundschecks.
+    ///
+    /// # Unsafe
+    /// Marked unsafe because
+    /// 1) the given range is NOT boundschecked
+    /// 2) The elements contained within the returned slice may be uninitialized, which is UB.
+    ///
+    /// # Examples
+    /// ```
+    /// use partialarray::MaybeArray;
+    ///
+    /// let mut array = MaybeArray::<usize, { 10 }>::default();
+    /// unsafe {
+    ///     // Initialize elements 3..6.
+    ///     for offset in 3..6 {
+    ///         array.write(offset, offset);
+    ///     }
+    ///     
+    ///     // This is ok, we get a slice to elements 3, 4 and 5.
+    ///     assert_eq!(array.slice_index(3..6), &[3, 4, 5]);
+    /// }
+    /// ```
+    #[inline]
     pub unsafe fn slice_index<R>(&self, r: R) -> &[T]
     where
         R: ops::RangeBounds<usize>,
@@ -164,6 +303,28 @@ impl<T, const N: usize> MaybeArray<T, { N }> {
         slice::from_raw_parts(self.nth_ptr(begin), end - begin)
     }
 
+    /// Returns a mutable subslice without doing boundschecks.
+    ///
+    /// # Unsafe
+    /// Marked unsafe because
+    /// 1) the given range is NOT boundschecked
+    /// 2) The elements contained within the returned slice may be uninitialized, which is UB.
+    /// # Examples
+    /// ```
+    /// use partialarray::MaybeArray;
+    ///
+    /// let mut array = MaybeArray::<usize, { 10 }>::default();
+    /// unsafe {
+    ///     // Initialize elements 3..6.
+    ///     for offset in 3..6 {
+    ///         array.write(offset, offset);
+    ///     }
+    ///     
+    ///     // This is ok, we get a slice to elements 3, 4 and 5.
+    ///     assert_eq!(array.slice_index_mut(3..6), &mut [3, 4, 5]);
+    /// }
+    /// ```
+    #[inline]
     pub unsafe fn slice_index_mut<R>(&mut self, r: R) -> &mut [T]
     where
         R: ops::RangeBounds<usize>,
@@ -185,7 +346,6 @@ impl<T, const N: usize> MaybeArray<T, { N }> {
 
         slice::from_raw_parts_mut(self.nth_ptr_mut(begin), end - begin)
     }
-
 }
 
 #[cfg(test)]
